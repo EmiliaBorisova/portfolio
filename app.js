e95773c6f4bcde41e07aae2bfdc2b7092e70fe75
Emilia Borisova <eboris@uw.edu>
2021-09-05 23:13:44 -0700

    loader, about, and resume sections done

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 "use strict";
 (function() {
     window.addEventListener("load", init);
-
+    
     function init() {
+        setTimeout(function() { 
+            let element = document.getElementsByClassName("loader");
+            element[0].classList.toggle("hide");
+            fadeout();
+        }, 1700);
         let menu = document.querySelector('#mobile-menu');
         let menuLinks = document.querySelector('.navbar-menu');
 
@@ -10,6 +15,22 @@
             menu.classList.toggle('is-active');
             menuLinks.classList.toggle('active');
         });
+    }
+
+    function fadeout() {
+        setInterval(hide, 100);
+    }
+
+    function hide() {
+        let wrapper = document.getElementsByClassName("loader-wrapper");
+        let opacity =  Number(window.getComputedStyle(wrapper[0]).getPropertyValue("opacity"));
+        if(opacity > 0) {
+            opacity = opacity-0.1;
+            wrapper[0].style.opacity=opacity;
+        } else {
+            clearInterval(0);
+            wrapper[0].style.zIndex=0;
+        }
     }
 })();
 
